08a0f4af0448f754838cfa77875f8a9658e2db25
John Cheng <devnull@johncheng.com>
2015-02-11 18:06:43 -0800

    some commentary about lack of rpm

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,5 +1,11 @@
 
 
+
+
+# for one reason or another, there are poker-eval packages for ubuntu but centos/rhel cannot
+# be easily found. someone (me?) will have to make them eventually? for now need to install
+# by source for centos/rhel :(
+
 case node['platform_family']
 when 'debian'
   default['pokenum']['packages'] = %w(libpoker-eval libpoker-eval-dev php5-dev php5-cli git) 
